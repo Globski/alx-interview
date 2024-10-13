4d07100eb35efc00359b400988bb844fb067533b
Gloriana <141042662+Globski@users.noreply.github.com>
2024-10-13 09:44:44 -0700

    Implement canUnlockAll function to determine if all boxes can be opened.

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 
-
 def canUnlockAll(boxes):
     """
     Determines if all boxes can be opened.
-    
+
     Args:
     boxes (list): A list of lists where each inner list contains keys to other boxes.
 
@@ -21,7 +20,7 @@ def canUnlockAll(boxes):
         if box_index not in visited:
             visited.add(box_index)
             for key in boxes[box_index]:
-                if key not in visited:
+                if key not in visited and key < len(boxes):
                     queue.append(key)
 
     return len(visited) == len(boxes)
