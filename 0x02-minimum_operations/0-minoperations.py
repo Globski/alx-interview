06ded162fed5688fd25d53ea73ab4488fb25a45d
Gloriana <141042662+Globski@users.noreply.github.com>
2024-10-17 08:02:08 -0700

    Add 0-minoperations.py: Function to calculate min operations.

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """
-This script calculates the minimum number of operations required 
-to achieve exactly n 'H' characters in a text editor using 
+This script calculates the minimum number of operations required
+to achieve exactly n 'H' characters in a text editor using
 only "Copy All" and "Paste" operations.
 """
+
 
 def minOperations(n):
     """
@@ -22,7 +23,7 @@ def minOperations(n):
 
     if n <= 1:
         return 0
-    
+
     operations = 0
     for i in range(2, n + 1):
         while n % i == 0:
